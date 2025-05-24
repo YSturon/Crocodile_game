63aafc7737e563eb631afdfbcc9c2949a1606f4e
Sturon <sturongame@gmail.com>
2025-05-24 17:46:52 +0500

    Добавил заглушку на WebSocket-контроллер

---
+++ server/src/main/java/ru/gesture/controller/LandmarkController.java
@@ -0,0 +1,21 @@
+package ru.gesture.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import ru.gesture.dto.ResultMessage;
+
+@Controller
+@RequiredArgsConstructor
+public class LandmarkController {
+
+    @MessageMapping("/landmarks")
+    @SendTo("/topic/result")
+    public ResultMessage handle(byte[] buf) {
+        // рандом
+        String animal = Math.random() < 0.5 ? "moose" : "bull";
+        float  conf   = (float) Math.random();
+        return new ResultMessage(animal, conf);
+    }
+}
