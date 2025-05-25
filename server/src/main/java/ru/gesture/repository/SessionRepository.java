1005092ad7e4afa62496957d9016a7bc571301c1
Sturon <sturongame@gmail.com>
2025-05-25 15:31:03 +0500

    .

--- server/src/main/java/ru/gesture/repository/SessionRepository.java
+++ server/src/main/java/ru/gesture/repository/SessionRepository.java
@@ -3,4 +3,9 @@ package ru.gesture.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.gesture.model.Session;
 
-public interface SessionRepository extends JpaRepository<Session, Long> {}
+import java.util.Optional;
+
+public interface SessionRepository extends JpaRepository<Session, Long> {
+
+    Optional<Session> findTopByUser_IdOrderByIdDesc(long userId);
+}
